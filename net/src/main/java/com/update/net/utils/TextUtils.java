baecf44ff17e0d223625ab2c713c1339465ece0d
liupu <575866447@qq.com>
2019-01-23 22:36:20 +0800

    文本工具类

---
+++ net/src/main/java/com/update/net/utils/TextUtils.java
@@ -0,0 +1,29 @@
+package com.update.net.utils;
+
+/**
+ * @author : liupu.
+ * @date : 2019/01/23
+ */
+public class TextUtils {
+
+    public static boolean isEmpty(CharSequence str) {
+        return str == null || str.length() == 0;
+    }
+
+    public static boolean equals(CharSequence a, CharSequence b) {
+        if (a == b) return true;
+        int length;
+        if (a != null && b != null && (length = a.length()) == b.length()) {
+            if (a instanceof String && b instanceof String) {
+                return a.equals(b);
+            } else {
+                for (int i = 0; i < length; i++) {
+                    if (a.charAt(i) != b.charAt(i)) return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
